b334e73789a2a9993b8844dd174014367bef3582
IgorPustylnik <darthveider36@gmail.com>
2025-03-22 00:36:46 +0300

    Change bpm field from int to float in sql db

--- sql/db.sql
+++ sql/db.sql
@@ -9,7 +9,7 @@ CREATE TABLE arrangements (
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
-    bpm INT NOT NULL,
+    bpm FLOAT NOT NULL,
     tags VARCHAR(1000) NOT NULL,
     file_name VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
